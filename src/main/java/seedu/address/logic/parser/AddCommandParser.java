d5be9f83fde2c812a880f3320f1c3251d3cdbe96
desnen <tohjunyee@gmail.com>
2026-03-12 23:39:55 +0800

    Update AddCommandParser.java

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -27,7 +27,7 @@ public class AddCommandParser implements Parser<AddCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddCommand parse(String args) throws ParseException {
-        // ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, 
+        // ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE,
         //                 PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
         // if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
         //         || !argMultimap.getPreamble().isEmpty()) {
