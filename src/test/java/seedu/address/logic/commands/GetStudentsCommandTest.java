dd561e6210a6a28b4f473ea03ebdb87914f278e6
Desnen Toh <desnentoh@Desnens-MacBook-Pro.local>
2026-04-06 15:38:36 +0800

    Codecov fix

---
+++ src/test/java/seedu/address/logic/commands/GetStudentsCommandTest.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class GetStudentsCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        GetStudentsCommand firstCommand = new GetStudentsCommand();
+        GetStudentsCommand  secondCommand = new GetStudentsCommand ();
+
+        // same object -> returns true
+        assertEquals(firstCommand, firstCommand);
+
+        // same values -> returns true
+        assertEquals(firstCommand, secondCommand);
+
+        // different types -> returns false
+        assertNotEquals(1, firstCommand);
+
+        // null -> returns false
+        assertNotEquals(null, firstCommand);
+    }
+}
